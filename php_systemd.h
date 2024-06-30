4c95479b66bd065e177a2cc0ad88f347ccb23f16
Ryan Coe <bluemrp9@gmail.com>
2024-06-30 10:03:22 -0700

    add sd_notify function
    Signed-off-by: Ryan Coe <bluemrp9@gmail.com>

--- php_systemd.h
+++ php_systemd.h
@@ -1,9 +1,10 @@
 #ifndef PHP_SYSTEMD_H
 #define PHP_SYSTEMD_H 1
-#define PHP_SYSTEMD_VERSION "0.1.2"
+#define PHP_SYSTEMD_VERSION "0.2.0"
 #define PHP_SYSTEMD_EXTNAME "systemd"
 
 PHP_FUNCTION(sd_journal_send);
+PHP_FUNCTION(sd_notify);
 
 extern zend_module_entry systemd_module_entry;
 #define phpext_systemd_ptr &systemd_module_entry
--- systemd.c
+++ systemd.c
@@ -1,12 +1,22 @@
-#ifdef HAVE_CONFIG_H
+    #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 #include "php.h"
 #include "php_systemd.h"
+#include <systemd/sd-daemon.h>
 #include <systemd/sd-journal.h>
 
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(sd_journal_send_args, 0, 1, _IS_BOOL, 0)
+	ZEND_ARG_VARIADIC_INFO(0, args)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(sd_notify_args, 0, 1, IS_LONG, 0)
+	ZEND_ARG_TYPE_INFO(0, state, IS_STRING, 0)
+ZEND_END_ARG_INFO()
+
 zend_function_entry systemd_functions[] = {
-    PHP_FE(sd_journal_send, NULL)
+    PHP_FE(sd_journal_send, sd_journal_send_args)
+    PHP_FE(sd_notify, sd_notify_args)
     {NULL, NULL, NULL} // Sentinel
 };
 
@@ -33,11 +43,11 @@ PHP_FUNCTION(sd_journal_send)
     zval *args;
     int argc, len, i;
     char *val;
-    
+
     if (zend_parse_parameters(ZEND_NUM_ARGS(), "+", &args, &argc) != SUCCESS) {
         return;
     }
-    
+
     // Allocate sufficient iovector space for the arguments.
     iov = safe_emalloc(argc, sizeof(struct iovec), 0);
     if (!iov) {
@@ -60,6 +70,20 @@ PHP_FUNCTION(sd_journal_send)
     // Free the iovector. The actual strings
     // are already managed by PHP.
     efree(iov);
-    
+
     RETURN_TRUE;
+}
+
+PHP_FUNCTION(sd_notify)
+{
+    zend_string *state = NULL;
+    int retval = 0;
+
+    if (zend_parse_parameters(ZEND_NUM_ARGS(), "S", &state) != SUCCESS) {
+        return;
+    }
+
+    retval = sd_notify(0, ZSTR_VAL(state));
+
+    RETURN_LONG(retval);
 }
